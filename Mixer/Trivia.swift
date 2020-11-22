8edd45ea1d0ec746a1b4af81380fa1bd2daa8787
Masthan Shaik <masthanshaik432@gmail.com>
2020-11-21 23:06:27 -0500

    commit

---
+++ Mixer/Trivia.swift
@@ -0,0 +1,29 @@
+//
+//  Trivia.swift
+//  Mixer
+//
+//  Created by Masthan Shaik on 11/21/20.
+//  Copyright © 2020 Devansh Yerpude. All rights reserved.
+//
+
+import Foundation
+
+class Question {
+    let questionImage: String
+    let question: String
+    let optionA: String
+    let optionB: String
+    let optionC: String
+    let optionD: String
+    let correctAnswer: Int
+    
+    init(image: String, questionText: String, choiceA: String, choiceB: String, choiceC: String, choiceD: String, answer: Int){
+        questionImage = image
+        question = questionText
+        optionA = choiceA
+        optionB = choiceB
+        optionC = choiceC
+        optionD = choiceD
+        correctAnswer = answer
+    }
+}
