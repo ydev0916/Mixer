b18194a3671ab0e0fd725ba62833fc7fc4a7602d
Devansh Yerpude <ydev0916@gmail.com>
2020-11-22 06:53:29 -0500

    addedquizviewcontroller

---
+++ Mixer/quizViewController.swift
@@ -0,0 +1,42 @@
+//
+//  quizViewController.swift
+//  Mixer
+//
+//  Created by Devansh Yerpude on 11/22/20.
+//  Copyright © 2020 Devansh Yerpude. All rights reserved.
+//
+
+import UIKit
+
+class quizViewController: UIViewController {
+
+    
+    @IBOutlet weak var question: UILabel!
+    
+    @IBOutlet weak var counter: UILabel!
+    
+    @IBOutlet weak var score: UILabel!
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
